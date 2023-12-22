12856b4d43a6f904ee2d7111fa8f3b7672d7bc21
Documenter.jl <documenter@juliadocs.github.io>
2023-12-22 22:02:13 +0000

    build based on d94016e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StorageComparison","category":"page"},{"location":"#StorageComparison","page":"Home","title":"StorageComparison","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StorageComparison.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StorageComparison]","category":"page"}]
+}
